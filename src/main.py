efad24ceb6a67f5cd0f0190b7b5d79e024cda856
SangWon7242 <gotkdqja@gmail.com>
2024-10-08 20:50:57 +0900

    키워드를 이용하여 원하는 뉴스 제목 가져오기

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ response = requests.get(url)
 if response.status_code == 200:
   print("웹 페이지를 성공적으로 가져왔습니다!")    
 
-
 # BeautifulSoup 객체 생성
 # response.text는 HTML 소스 코드 텍스트임
 html_data = BeautifulSoup(response.text, 'html.parser')  
@@ -29,6 +28,25 @@ for news_title in headline_news_list:
 # print(news_title_list)  
 
 # 2차 가공
+'''
 for i, title in enumerate(news_title_list):
   no = i + 1
   print(f"{no} : {title}")
+'''  
+
+
+# 특정 키워드로 검색 후, 검색 결과 리스트 담고 출력
+find_keyword_news_list = []  
+
+keyword = '미국'
+for i, title in enumerate(news_title_list):
+  # no = i + 1
+  # print(f"{no} : {title}")
+  
+  if title.find(keyword) != -1:
+    find_keyword_news_list.append(title)
+  
+print(find_keyword_news_list) # 특정 키워드로 걸러진 데이터가 리스트에 저장
+
+for i, title in enumerate(find_keyword_news_list):
+  print(f"{i + 1} : {title} ")
